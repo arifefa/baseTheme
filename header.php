35f98ae4c5f8a2e0bbe4f5d1089ee7325c2d854c
arifefa <baimhabib@gmail.com>
2024-10-22 01:50:06 +0000

    Upload New File

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sreatch</title>
+</head>
+<body>
+    <header>
+        <?php wp_nav_menu(); ?>
+        <?php wp_head(); ?>
+    </header>
+    
