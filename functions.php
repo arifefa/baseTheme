2f3057c22b2b446bb24c3f2217972a56df13b8a9
arifefa <baimhabib@gmail.com>
2024-10-24 00:14:01 +0700

    resgister menu

--- functions.php
+++ functions.php
@@ -1,14 +1,26 @@
 <?php 
-// register css
+// =========================
+// LOAD CSS
+// =========================
 function versioning_style(){
 	wp_enqueue_style('custom-style', get_stylesheet_directory_uri() . '/style.css', array(), filemtime(get_stylesheet_directory() . '/style.css'), null);
 }
 add_action('wp_print_styles', 'versioning_style');
 
-// enable menu and widget in apperance
-add_theme_support( 'menus' );
-add_theme_support( 'widgets');
+// =========================
+// ENABLE MENU IN APPERANCE
+// =========================
+add_action('after_setup_theme',function(){
+    add_theme_support( 'menus' );
+    register_nav_menus( array(
+        'main_menu' => 'Header navigation menu',
+        'footer_menu' => 'Footer navigation menu',
+    ));
+});
 
+// =========================
+// SHOW DATA POST
+// =========================
 add_shortcode('show_bestarticle',function(){
     $args = array(
         'post_type'=> 'post',
